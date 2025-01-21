9313cb91a713a3a59a84ced320ace76e991be82a
= <=>
2025-01-21 20:07:53 +0400

    player, asteroids and collisions

--- main.py
+++ main.py
@@ -1,17 +1,49 @@
 import pygame
+from asteroid import Asteroid
 import constants as C
+from player import Player
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
-    pygame.display.set_mode((C.SCREEN_WIDTH, C.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((C.SCREEN_WIDTH, C.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    player = Player(C.SCREEN_WIDTH/2, C.SCREEN_HEIGHT/2)
+    AsteroidField()
+
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for obj in updatable:
+            obj.update(dt)
 
-        surface = pygame.display.get_surface()
-        surface.fill("black")
+        for obj in asteroids:
+            if obj.is_collision(player):
+                print("Game Over!")
+                exit()
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+
     print("Starting asteroids!")
     print(f"Screen width: {C.SCREEN_WIDTH}")
     print(f"Screen height: {C.SCREEN_HEIGHT}")
